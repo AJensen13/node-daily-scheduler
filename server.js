5acf841916886ffa7d2a4749f37febfc67cfafbd
Andrew Jensen <jensen.andrew13@gmail.com>
2018-01-02 16:24:30 -0700

    Added Gulp file
    Includes  a separate express server file, updated gulp tasks to watch the js files and run the server on the "serve" command.

---
+++ server.js
@@ -0,0 +1,24 @@
+var chalk = require('chalk'),
+    express = require('express');
+
+var app = express();
+
+app.use(express.static('views'));
+
+app.get('/', function(req, res) {
+    res.send('index.html');
+});
+
+app.get('/:id/profile/', function(req, res){
+
+});
+
+app.get('/:id/calendar/', function(req, res){
+
+});
+
+// app.get('/:id/', function(req, res){
+
+// });
+
+app.listen(3000);
