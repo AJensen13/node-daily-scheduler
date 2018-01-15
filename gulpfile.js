990a085091386620c0b831873fa09b327d0daa2c
Andrew Jensen <jensen.andrew13@gmail.com>
2018-01-15 16:41:46 -0700

    UI Changes
    Made changes to profile page, added gulp-sass package and set up sass watch for compiling.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 var gulp = require('gulp'),
     nodemon = require('gulp-nodemon'),
     jshint = require('gulp-jshint'),
-    livereload = require('gulp-livereload');
+    livereload = require('gulp-livereload'),
+    sass = require('gulp-sass');
 
 gulp.task('scripts', function() {  
   return gulp.src('public/js/*.js')
@@ -10,15 +11,22 @@ gulp.task('scripts', function() {
     .pipe(livereload());
 });
 
-gulp.task('html',function(){  
+gulp.task('ejs',function(){  
   return gulp.src('views/**/*.ejs')
-  .pipe(livereload());
+    .pipe(livereload());
+});
+
+gulp.task('sass', function(){
+  return gulp.src('./src/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./public/css'));
 });
 
 gulp.task('watch', function() {  
   livereload.listen();
   gulp.watch('public/js/*.js', ['scripts']);
   gulp.watch('views/**/*.ejs', ['ejs']);
+  gulp.watch('./src/sass/**/*.scss', ['sass']);
 });
 
 gulp.task('server',function(){  
